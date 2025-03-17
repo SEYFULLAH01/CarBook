d2060b4694addf784c520c3e5cd1deecc5100c3c
SEYFULLAH01 <120310366+SEYFULLAH01@users.noreply.github.com>
2025-03-17 21:33:20 +0300

    3.bölüm sonu

---
+++ Core/CarBook.Application/Features/CQRS/Queries/AboutQueries/GetAboutByIdQuery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.CQRS.Queries.AboutQueries
+{
+    public class GetAboutByIdQuery
+    {
+        public int AboutID { get; set; }
+
+        public GetAboutByIdQuery(int aboutID)
+        {
+            AboutID = aboutID;
+        }
+    }
+}
