dbc0b3a692dbe4aef3195ca0440170baf6d6d0b3
SEYFULLAH01 <120310366+SEYFULLAH01@users.noreply.github.com>
2025-03-19 23:57:56 +0300

    7.bölüm Sonu

---
+++ Frontends/CarBook.WebUI/ViewComponents/TestimonialViewComponents/_TestimonialComponantPartial.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.ViewComponents.TestimonialViewComponents
+{
+    public class _TestimonialComponantPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _TestimonialComponantPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7139/api/Testimonial");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
