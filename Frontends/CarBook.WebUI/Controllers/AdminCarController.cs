25fa08a4a1166455572040ac6e5c32d01b030220
SEYFULLAH01 <120310366+SEYFULLAH01@users.noreply.github.com>
2025-03-28 15:56:36 +0300

    14. bölüm 110 dan devam edilecek yeni araç girişi yapılacak

---
+++ Frontends/CarBook.WebUI/Controllers/AdminCarController.cs
@@ -0,0 +1,29 @@
+﻿using CarBook.Dto.CarDtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace CarBook.WebUI.Controllers
+{
+    public class AdminCarController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public AdminCarController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7139/api/Cars/GetCarWithBrand");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCarWithBrandsDtos>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
