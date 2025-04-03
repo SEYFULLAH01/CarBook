9044b365f88d602f563f6122755c59d77c1802a1
SEYFULLAH01 <120310366+SEYFULLAH01@users.noreply.github.com>
2025-04-03 23:22:55 +0300

    Filtre detayları yapıldı seçili lokasyona göre araç getirme yapılacak

--- Frontends/CarBook.Dto/RentACarDtos/FilterRentACarDto.cs
+++ Frontends/CarBook.Dto/RentACarDtos/FilterRentACarDto.cs
@@ -8,10 +8,7 @@ namespace CarBook.Dto.RentACarDtos
 {
     public class FilterRentACarDto
     {
-        public int carID { get; set; }
-        public string Brand { get; set; }
-        public string Model { get; set; }
-        public decimal Amount { get; set; }
-        public string CoverImageUrl { get; set; }
+        public int locationID { get; set; }
+        public bool available { get; set; }
     }
 }
--- Frontends/CarBook.WebUI/Controllers/RentACarListController.cs
+++ Frontends/CarBook.WebUI/Controllers/RentACarListController.cs
@@ -9,10 +9,36 @@ namespace CarBook.WebUI.Controllers
 {
     public class RentACarListController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public RentACarListController(IHttpClientFactory httpClientFactory)
         {
-            var data = TempData["value"];
-            ViewBag.v = data;
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index(FilterRentACarDto filterRentACarDto)
+        {
+            var bookpickdate = TempData["bookpickdate"];
+            var bookoffdate = TempData["bookoffdate"];
+            var timepick = TempData["timepick"];
+            var timeoff = TempData["timeoff"];
+            var locationID = TempData["locationID"];
+
+            filterRentACarDto.locationID = int.Parse(locationID.ToString());
+            filterRentACarDto.available = true;
+
+            ViewBag.bookpickdate = bookpickdate;
+            ViewBag.bookoffdate = bookoffdate;
+            ViewBag.timepick = timepick;
+            ViewBag.timeoff = timeoff;
+            ViewBag.locationID = locationID;
+
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(filterRentACarDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7139/api/RentACars", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
             return View();
         }
     }
